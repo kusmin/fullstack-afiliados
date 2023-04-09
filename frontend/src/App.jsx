59c48bfa1d5697675547a22601fe91299e1ad184
Renan Ribeiro <renan.lagee@gmail.com>
2023-04-09 19:17:34 -0300

    ajuste na pagina app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,24 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { Route, BrowserRouter as Router, Routes, useNavigate } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import SignUp from "./pages/SignUp";
+import AuthService from './service/AuthService';
 import { login } from './store/slices/authSlice';
 
 const App = () => {
   const dispatch = useDispatch();
-  const navigate = useNavigate();
+  const [loggedIn, setLoggedIn] = useState(false);
+
 
   useEffect(() => {
     const loggedInUser = AuthService.usuarioCorrente();
     if (loggedInUser) {
       dispatch(login(loggedInUser));
+      setLoggedIn(true);
     } else {
-      if (location.pathname !== "/login" && location.pathname !== "/signup") { 
-        navigate("/home");
-      }
-      
+      setLoggedIn(false);
     }
   }, [dispatch]);
 
@@ -27,7 +27,7 @@ const App = () => {
     <Routes>
       <Route
         path="/"
-        render={() => (loggedIn ? <Redirect to="/home" /> : <Redirect to="/login" />)}
+        element={<Home />} 
         />
         <Route path="/signup" element={<SignUp />} />
       <Route path="/login" element={<Login />} />
